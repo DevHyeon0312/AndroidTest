e98705fb87d95b80985da2302397bb6f1e7f79da
devhyeon <devhyeon0312@gmail.com>
2021-10-08 17:48:43 +0900

    DataBinding activity variable remove

--- app/src/main/java/com/devhyeon/uitest/main/MainActivity.kt
+++ app/src/main/java/com/devhyeon/uitest/main/MainActivity.kt
@@ -18,7 +18,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.activity = this@MainActivity
         binding.lifecycleOwner = this@MainActivity
         binding.viewModel = mainViewModel
     }
